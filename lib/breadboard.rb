2a8fdf78e790414b00fa9914c8b608c335f3699a
Dorren Chen <dorrenchen@gmail.com>
2010-09-29 12:24:40 -0400

    make it work in rails3

--- lib/breadboard.rb
+++ lib/breadboard.rb
@@ -1,4 +1,4 @@
 require 'active_resource'
 require 'active_support'
 require 'breadboard/breadboard'
-BREAD_BOARD = Breadboard.new((File.open(RAILS_ROOT + "/config/breadboard.yml") rescue "")) if defined?(RAILS_ROOT)
+BREAD_BOARD = Breadboard.new((File.open(RAILS_ROOT.to_s + "/config/breadboard.yml") rescue "")) if defined?(RAILS_ROOT)
