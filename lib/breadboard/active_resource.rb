13f57633e1e81654110936818ff44a787bb45f59
Matt Parker <moonmaster9000@gmail.com>
2011-04-27 11:37:34 -0400

    connection needs be at least defined for active_resource to work

--- lib/breadboard/active_resource.rb
+++ lib/breadboard/active_resource.rb
@@ -5,7 +5,13 @@ module ActiveResource
         @site || Breadboard.service_for(self)
       end
 
+      def connection_with_breadboard(reset=false)
+        @connection ||= nil
+        connection_without_breadboard(reset)
+      end
+
       alias_method_chain :site, :breadboard
+      alias_method_chain :connection, :breadboard
     end
   end
 end
