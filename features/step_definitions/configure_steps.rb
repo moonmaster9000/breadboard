4e0f6a1a8c47200e721909c3036d1e3a2657e8e6
Dorren Chen <dorrenchen@gmail.com>
2011-11-28 17:29:49 -0500

    convert site to URI object

--- features/step_definitions/configure_steps.rb
+++ features/step_definitions/configure_steps.rb
@@ -132,6 +132,7 @@ Then /^I should be able to configure model with user, password$/ do
     end
   end
   
+  Venue.site.should be_kind_of URI
   Venue.site.to_s.should == "http://livesite"
   Venue.password.should == "secret"
 end
